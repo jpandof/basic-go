430f80c768bee04ee0783e71ee30be08b05f56ed
Jesus Pando-Fernandez <jesus.pando-fernandez@orchestra.eu>
2020-01-27 15:29:20 +0100

    Metodos struct

--- myType/myType.go
+++ myType/myType.go
@@ -17,10 +17,21 @@ type IronMan struct {
 	SuperPoder string
 }
 
+func (i *IronMan) GetBatery() int {
+	return i.Batery * 2
+}
+
+func (i *IronMan) getSuperPoder() string {
+	return i.SuperPoder + "_fly"
+}
+
 func main() {
 	i := IronMan{Man{"Pando"}, Robot{78, "Jesus"}, "fuego"}
+	fmt.Printf("Batery, %v\n", i.GetBatery())
+	fmt.Printf("Batery, %v\n", i.Batery)
+	fmt.Printf("SuperPoder, %v\n", i.getSuperPoder())
 	fmt.Printf("SuperPoder, %v\n", i.SuperPoder)
 	fmt.Printf("Name man, %v\n", i.Man.Name)
 	fmt.Printf("Name robot, %v\n", i.Robot.Name)
-	fmt.Printf("Batery, %v\n", i.Batery)
+
 }
